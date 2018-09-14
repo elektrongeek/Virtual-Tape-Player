a8071b5821e6e1955236c5e0f825b760f9ffc1e0
Jérémy <jerllinares@aol.com>
2018-09-15 00:09:22 +0200

    1st Commit , Load button works as well as playback

---
+++ Tape Player/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Tape_PLayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Load_File = new System.Windows.Forms.Button();
+            this.File_Dialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Load_File
+            // 
+            this.Load_File.Location = new System.Drawing.Point(12, 176);
+            this.Load_File.Name = "Load_File";
+            this.Load_File.Size = new System.Drawing.Size(113, 73);
+            this.Load_File.TabIndex = 0;
+            this.Load_File.Text = "LOAD";
+            this.Load_File.UseVisualStyleBackColor = true;
+            this.Load_File.Click += new System.EventHandler(this.Load_File_Click);
+            // 
+            // File_Dialog
+            // 
+            this.File_Dialog.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Player
+            // 
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(120, 12);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(226, 111);
+            this.Player.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 261);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Load_File);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Load_File;
+        private System.Windows.Forms.OpenFileDialog File_Dialog;
+        private System.Windows.Forms.Label label1;
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+    }
+}
+
